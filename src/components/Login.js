6646dd50deb45952e9c11a1890e167305d647615
Sagar0120 <20it004@charusat.edu.in>
2022-09-13 12:35:33 +0530

    SignIn function added

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,39 @@ export default function Login() {
 
   return (
     <div>
-      
+      <section>
+      <form>
+        <h5>Sign In</h5>
+        <div>
+          <label>Phone Number</label>
+          <div>
+            <input
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div>
+          <label>Password</label>
+          <div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+
+        <button type="submit" onClick={Login}>
+          Sign in
+        </button>
+      </form>
+      </section>
     </div>
   );
 }
