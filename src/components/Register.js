782a990c78cf0dcdb163c0cd8778a5408eb27647
Sagar0120 <20it004@charusat.edu.in>
2022-09-13 12:37:50 +0530

    Register created completed

--- src/components/Register.js
+++ src/components/Register.js
@@ -41,7 +41,50 @@ class Register extends React.Component {
   render() {
     return (
       <>
-        
+        <section>
+          <form>
+            <label>Name : </label>
+            <input
+              placeholder="Name"
+              name="name"
+              className="form-control"
+              value={this.state.name}
+              onChange={this.changenamehandler}
+            />
+
+            <label>Email : </label>
+            <input
+              placeholder="Email"
+              name="email"
+              className="form-control"
+              value={this.state.email}
+              onChange={this.changemailhandler}
+            />
+
+            <label>Phone : </label>
+            <input
+              placeholder="phone"
+              name="phone"
+              className="form-control"
+              value={this.state.phone}
+              onChange={this.changephonehandler}
+            />
+
+            <label>Password : </label>
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              className="form-control"
+              value={this.state.password}
+              onChange={this.changepasshandler}
+            />
+
+            <button className="btn btn-success" onClick={this.saveUser}>
+              Create user
+            </button>
+          </form>
+        </section>
 
       </>
     );
