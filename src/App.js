4516fff35962326f7fbe6d4b926ff1b998fbca27
Sagar0120 <20it004@charusat.edu.in>
2022-09-14 12:21:45 +0530

    change in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 //import Register from './components/Register';
 import Login from './components/Login';
 import './App.css';
-
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
+    <div> 
       <Login/>
-
-      
     </div>
+    </BrowserRouter>
   );
 }
 
